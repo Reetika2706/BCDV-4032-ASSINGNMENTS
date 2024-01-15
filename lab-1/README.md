9aa33c0c62bda76387c9df636e63b0dd4072fbe3
Reetika <ritikajibansal@gmail.com>
2024-01-14 20:46:39 -0500

    lab1

---
+++ lab-1/README.md
@@ -0,0 +1,2 @@
+#Docker hub Image Url
+https://hub.docker.com/repository/docker/reetika2706/my-docker-app/general
---
+++ lab-1/my-docker-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Name - Reetika
+        </p>
+        <p>
+          Student Id - 101496084
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
